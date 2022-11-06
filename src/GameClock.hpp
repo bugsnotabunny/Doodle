68938b06c1f1c0a69e771e560144bae135f62680
bugsnotabunny <66078473+bugsnotabunny@users.noreply.github.com>
2022-11-06 08:38:15 +0300

    Update GameClock.hpp

--- src/GameClock.hpp
+++ src/GameClock.hpp
@@ -11,8 +11,15 @@ namespace ddl
   {
   public:
     GameClock();
+    GameClock(const GameClock&) = default;
+    GameClock(GameClock&&) = default;
+
+    GameClock& operator=(const GameClock&) = default;
+    GameClock& operator=(GameClock&&) = default;
+
     sf::Time getTimeFromLaunch();
     sf::Time getTimeFromUpdate();
+
     void update() override;
   private:
     sf::Clock fromLaunch_;
