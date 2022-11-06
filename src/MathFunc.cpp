9d78d7cd74f8d871028406aaf30dcc651c0f9774
bugsnotabunny <66078473+bugsnotabunny@users.noreply.github.com>
2022-11-07 02:31:17 +0300

    impl random generator

--- src/MathFunc.cpp
+++ src/MathFunc.cpp
@@ -1,8 +1,19 @@
 #include "MathFunc.hpp"
 
 #include <cmath>
+#include <random>
 
 float ddl::getVectorAbs(const sf::Vector2f& vector) noexcept
 {
   return std::sqrt(vector.x * vector.x + vector.y * vector.y);
+}
+
+ddl::Random::Random(int start, int end, int seed) noexcept:
+  gen_(seed),
+  distr_(start, end)
+{}
+
+int ddl::Random::get() noexcept
+{
+  return distr_(gen_);
 }
--- src/MathFunc.hpp
+++ src/MathFunc.hpp
@@ -1,11 +1,23 @@
 #ifndef MATH_FUNC_HPP
 #define MATH_FUNC_HPP
 
+#include <random>
+
 #include <SFML/System/Vector2.hpp>
 
 namespace ddl
 {
   float getVectorAbs(const sf::Vector2f& vector) noexcept;
+
+  class Random
+  {
+  public:
+    Random(int start, int stop, int seed) noexcept;
+    int get() noexcept;
+  private:
+    std::mt19937 gen_;
+    std::uniform_int_distribution<> distr_;
+  };
 }
 
 #endif
