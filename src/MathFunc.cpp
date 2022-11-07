45548e920d35b012b7cc3dcbc431419949829388
bugsnotabunny <66078473+bugsnotabunny@users.noreply.github.com>
2022-11-07 07:09:08 +0300

    random num generator made

--- src/MathFunc.cpp
+++ src/MathFunc.cpp
@@ -7,13 +7,3 @@ float ddl::getVectorAbs(const sf::Vector2f& vector) noexcept
 {
   return std::sqrt(vector.x * vector.x + vector.y * vector.y);
 }
-
-ddl::Random::Random(int start, int end, int seed) noexcept:
-  gen_(seed),
-  distr_(start, end)
-{}
-
-int ddl::Random::get() noexcept
-{
-  return distr_(gen_);
-}
--- src/MathFunc.hpp
+++ src/MathFunc.hpp
@@ -9,15 +9,28 @@ namespace ddl
 {
   float getVectorAbs(const sf::Vector2f& vector) noexcept;
 
+  template < typename int_t >
   class Random
   {
   public:
-    Random(int start, int stop, int seed) noexcept;
+    Random(int_t start, int_t stop, int seed) noexcept;
     int get() noexcept;
   private:
     std::mt19937 gen_;
-    std::uniform_int_distribution<> distr_;
+    std::uniform_int_distribution< int_t > distr_;
   };
+}
+
+template < typename int_t >
+ddl::Random< int_t >::Random(int_t start, int_t end, int seed) noexcept:
+  gen_(seed),
+  distr_(start, end)
+{}
+
+template < typename int_t >
+int ddl::Random< int_t >::get() noexcept
+{
+  return distr_(gen_);
 }
 
 #endif
