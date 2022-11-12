d78477344e8926cddf4b296a01b7f750180d08f6
bugsnotabunny <66078473+bugsnotabunny@users.noreply.github.com>
2022-11-12 03:39:42 +0300

    change fps to 120

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main()
   {
     sf::RenderWindow window{};
     window.create(sf::VideoMode(ddl::gameWidth, ddl::gameHeight, 32), "Doodle Jump (not really)", sf::Style::Titlebar | sf::Style::Close);
-    ddl::run(window, 60);
+    ddl::run(window, 120);
   }
   catch (const std::exception & e)
   {
