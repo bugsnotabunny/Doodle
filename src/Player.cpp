5452ce8e86fdefb7caca4fef60c6ec8c5e882c64
bugsnotabunny <66078473+bugsnotabunny@users.noreply.github.com>
2022-11-12 03:32:56 +0300

    more self-contained player

--- src/Player.cpp
+++ src/Player.cpp
@@ -3,19 +3,33 @@
 #include <functional>
 #include <utility>
 
+#include "InputTexture.hpp"
+
 namespace
 {
+  const sf::Texture texture = ddl::inputTexture("doodler.png");
+
   const sf::Vector2f playerSpeedLimit = {5000, 10000};
   const sf::Vector2f jumpSpeed = {0, -900};
+
+  const float gravity = 1000;
+  const float horizontalAccelerationBase = 1000;
 }
 
-ddl::Player::Player(sf::Sprite&& sprite):
-  Base(std::move(sprite)),
-  speed({0, 0}, playerSpeedLimit)
-{}
+ddl::Player::Player(const sf::Vector2f& pos):
+  Base(sf::Sprite{texture}),
+  speed({0, 0}, playerSpeedLimit),
+  acceleration_({0, gravity}),
+  xAccelerationDirection_(Stop)
+{
+  setScale(0.5, 0.5);
+  setPosition(pos);
+}
 
 void ddl::Player::update(float deltaTime)
 {
+  acceleration_.x = horizontalAccelerationBase * xAccelerationDirection_;
+  speed += acceleration_ * deltaTime;
   auto diff = speed.getVec() * deltaTime;
   Base::move(diff);
 }
@@ -23,4 +37,9 @@ void ddl::Player::update(float deltaTime)
 void ddl::Player::jump() noexcept
 {
   speed.setVec(jumpSpeed);
+}
+
+void ddl::Player::setMoveDirection(Direction xDirection) noexcept
+{
+  xAccelerationDirection_ = xDirection;
 }
--- src/Player.hpp
+++ src/Player.hpp
@@ -9,17 +9,29 @@
 
 namespace ddl
 {
+  enum Direction: char
+  {
+    Left = -1,
+    Stop,
+    Right
+  };
+
   class Player: public RenderableObject, public IUpdatable
   {
     using Base = RenderableObject;
   public:
-    Player(sf::Sprite&& sprite);
+    Player(const sf::Vector2f& pos);
     virtual ~Player() = default;
 
     virtual void update(float deltaTime) override;
     void jump() noexcept;
+    void setMoveDirection(Direction xAccelerationDirection) noexcept;
 
+  private:
+    using AccelerationT = Acceleration< LimitedVector< float >, sf::Vector2f >;
     LimitedVector< float > speed;
+    sf::Vector2f acceleration_;
+    Direction xAccelerationDirection_;
   };
 }
 
